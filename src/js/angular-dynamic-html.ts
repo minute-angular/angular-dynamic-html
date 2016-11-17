0913769ae7059b133f9e5db94b3c6fb705dd965a
Sanchit <dev@minutephp.com>
2016-11-17 19:44:55 +0530

    updated to version v0.0.4

--- src/js/angular-dynamic-html.ts
+++ src/js/angular-dynamic-html.ts
@@ -5,18 +5,18 @@ module Minute {
     export class DynamicHtml implements ng.IDirective {
         restrict = 'A';
         replace = true;
-        scope = {dynamicHtml: '=?'};
+        scope: any = {dynamicHtml: '=?'};
 
-        constructor(private $compile:ng.ICompileService) {
+        constructor(private $compile: ng.ICompileService) {
         }
 
-        static factory():ng.IDirectiveFactory {
-            var directive:ng.IDirectiveFactory = ($compile:ng.ICompileService) => new DynamicHtml($compile);
+        static factory(): ng.IDirectiveFactory {
+            var directive: ng.IDirectiveFactory = ($compile: ng.ICompileService) => new DynamicHtml($compile);
             directive.$inject = ["$compile"];
             return directive;
         }
 
-        link = ($scope:any, element:ng.IAugmentedJQuery, attrs:ng.IAttributes) => {
+        link = ($scope: any, element: ng.IAugmentedJQuery, attrs: ng.IAttributes) => {
             $scope.$watch('dynamicHtml', (html, last) => {
                 element.html(html);
                 this.$compile(element.contents())($scope);
